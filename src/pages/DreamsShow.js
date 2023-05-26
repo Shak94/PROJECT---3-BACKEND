8653c79bf5430e4f7b4533a08b7be0ede3885caf
Shak94 <shakoi.1994@gmail.com>
2023-05-26 11:44:23 -0400

    Working on dream show page

--- src/pages/DreamsShow.js
+++ src/pages/DreamsShow.js
@@ -3,6 +3,17 @@ import { useParams } from "react-router";
 import { Link } from "react-router-dom";    
 
 function DreamsShow (){
+    const[dream, setDream] = useState([]);
+    const dreamID = params.useParams();
+    async function getDream(){
+        try{
+            let singleDream = await fetch(`http://localhost:4000/dreams/${dreamId}`)
+            singleDream = await singleDream.json();
+            setDream(singledream);
+        } catch(error){
+            console.log(error)
+        }
+    }
     return(
  
     <>
--- src/pages/NightmaresIndex.js
+++ src/pages/NightmaresIndex.js
@@ -1,14 +1,106 @@
 import { Link } from "react-router-dom";
-import { useState,useEff } from "react";
+import { useState, useEffect } from "react";
 
+function NightmaresIndex() {
+  const [nightmares, setNightmares] = useState([]);
 
-function NightmaresIndex(){
-    return(
+  const [nightmaresForm, setNightmaresForm] = useState({
+    title: "",
+    meaning: "",
+    image: "",
+  });
 
-    <> 
-   
-    <h1> My Nightmare index</h1>
+  async function getNightmares() {
+    try {
+      let apiNightmares = await fetch("http://localhost:4000/nightmares");
+      apiNightmares = await apiNightmares.json();
+      setNightmares(apiNightmares);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getNightmares();
+  }, []);
+
+  function loader(arr) {
+    return (
+      <>
+        {arr.map((nightmare, idx) => {
+          return (
+            <div key={idx}>
+              <Link to={`/nightmares/${nightmare._id}`}>
+                <h1>Title: {nightmare.title}</h1>
+              </Link>
+              <h2>Meaning: {nightmare.meaning}</h2>
+              <img src={nightmare.image} alt="Nightmare Name" />
+            </div>
+          );
+        })}
+      </>
+    );
+  }
+
+  function handleChange(e) {
+    console.log(e.target);
+    setNightmaresForm((previousFormState) => ({
+      ...previousFormState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  async function handleSubmit(e) {
+    try {
+      e.preventDefault();
+      await fetch("http://localhost:4000/nightmares", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(nightmaresForm),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <h1>My Nightmare index</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Title:</label>
+          <input
+            type="text"
+            name="title"
+            onChange={handleChange}
+            placeholder="Nightmare"
+          />
+        </div>
+        <div>
+          <label>Meaning:</label>
+          <input
+            type="text"
+            name="meaning"
+            onChange={handleChange}
+            placeholder="Nightmare's Meaning"
+          />
+        </div>
+        <div>
+          <label>Image:</label>
+          <input
+            type="text"
+            name="image"
+            onChange={handleChange}
+            placeholder="Nightmares Image"
+          />
+        </div>
+        <button>Add Nightmare</button>
+      </form>
+      {nightmares.length ? loader(nightmares) : <h3>One Moment</h3>}
     </>
-    )
-}   
+  );
+}
+
 export default NightmaresIndex;
