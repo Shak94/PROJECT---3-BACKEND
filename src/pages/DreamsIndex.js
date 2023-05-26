6bfd95e5f8436e3ce3160a0b51d410af69fcbff9
Shak94 <shakoi.1994@gmail.com>
2023-05-26 10:53:56 -0400

    Working on Dreams Index/ Form

--- src/pages/DreamsIndex.js
+++ src/pages/DreamsIndex.js
@@ -14,7 +14,7 @@ function DreamsIndex(){
     async function getDreams(){
         try{
             let apiDreams = await fetch('http://localhost:4000/dreams')
-            apiDreams = await apiDreams.json
+            apiDreams = await apiDreams.json()
             SetDreams(apiDreams);
         } catch (error){
             console.log(error);
@@ -49,30 +49,50 @@ function DreamsIndex(){
     <h1> My dream index</h1>
     </>
     )
+    function handleChange(e){
+        console.log(e.target)
+        setDreamsForm((previousFormState) =>({
+            ...previousFormState,
+            [e.target.name]: e.target.value
+        }))
+        }
+        
+        async function handleSubmit(e){
+            try{
+                e.preventDefault();
+                await fetch ('http://localhost:4000/dreams',{
+                    method:"POST",
+                   headers:{
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(dreamsForm)
+                })
+            } catch (error){
+        
+                console.log(error)
+            }
+            
+        }
+        return(
+            <>
+            <form onSubmit={handleSubmit}>
+                <label>Title:</label>
+                <input type ="text" name="title" onChange={handleChange} placeholder="Dream"/>
+                <label> Meaning:</label>
+                <input type ="text" name="meaning" onChange={handleChange} placeholder="Dream's Meaning"/>
+                <label>Image:</label>
+                <input type ="text" name="Image" onChange={handleChange} placeholder="Dream's Image"/>
+                <button> Add Dream</button>
+
+            </form>
+            {dreams.length ? useLoaderData(dreams):<h3> One Moment</h3>}
+            </>
+        )
 }   
 
-function handleChange(e){
-console.log(e.target)
-setDreamsForm((previousFormState) =>({
-    ...previousFormState,
-    [e.target.name]: e.target.value
-}))
-}
 
-async function handleSubmit(e){
-    try{
-        e.preventDefault();
-        await fetch ('http://localhost:4000/dreams',{
-            method:"POST",
-           headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(dreamsForm)
-        })
-    } catch (error){
 
-        console.log(error)
-    }
-}
+
+
 
 export default DreamsIndex;
